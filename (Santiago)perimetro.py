81008d9ac01ae00af75569d8ca4d39e2a3b707ef
AurumDAfpel <santiagogomezrojas2007@gmail.com>
2025-10-15 20:05:14 -0500

    feat: se completo el menu principal de perimetros
    se completo el  menu inicial de la seccion de perimetros para el proyecto de calculadora

---
+++ (Santiago)perimetro.py
@@ -0,0 +1,20 @@
+
+
+
+
+def menuPerimetros():
+    """bienvenido al menu de perimetros
+1. elije la figura que le quieras sacar perimetro
+2. salir """
+
+    opcion = input(" ")
+ 
+    if opcion <1 or opcion >2:
+        print("opcion invalida intenta denuevo")
+    elif opcion == 1:
+        elegirFigura()("en desarrollo")
+    elif opcion == 2:
+        return
+
+
+
