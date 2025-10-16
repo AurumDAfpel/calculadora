026615dcaff11a51fceee449cdd483580d0464eb
AurumDAfpel <santiagogomezrojas2007@gmail.com>
2025-10-15 20:37:06 -0500

    feat: se implemento la funcion perimetro
    se implemento la funcion perimetros correctamente, se reemplazo el archivo perimetro.py con un modulo aparte que contiene las mismas funciones

---
+++ calculadora-matematicas/modulos/perimetro/cuadrado.py
@@ -0,0 +1,5 @@
+def perimetroCuadrado():
+    lado = float(input("ingresa el lado de tu cuadrado: "))
+    perimetro = lado + lado + lado + lado
+    print(f"el perimetor de este cuadrado es {perimetro}")
+    return 
---
+++ calculadora-matematicas/modulos/perimetro/mainPerimetro.py
@@ -0,0 +1,28 @@
+from cuadrado import perimetroCuadrado
+from circulo import perimetroCirculo
+from rectangulo import perimetroRectangulo
+from triangulo import perimetroTriangulo
+
+def elegirFigura():
+    """bienvenido al menu de perimetros
+elije una figura:
+1. cuadrado
+2. triangulo
+3. circulo
+4. rectangulo
+0. salir
+"""
+    opcion = input(" ")
+
+    if opcion <0 or opcion >4:
+        print("opcion invalida intenta denuevo")
+    if opcion == 1:
+        perimetroCuadrado()
+    elif opcion == 2:
+        perimetroTriangulo()
+    elif opcion == 3:
+        perimetroCirculo()
+    elif opcion == 4:
+        perimetroRectangulo()
+    else:
+        return
