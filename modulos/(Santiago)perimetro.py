564eaeb913e33325ccd58b7789b28682c6526fc9
AurumDAfpel <santiagogomezrojas2007@gmail.com>
2025-10-15 19:44:17 -0500

    feat: creacion del menu perimetros

---
+++ modulos/(Santiago)perimetro.py
@@ -0,0 +1,14 @@
+
+
+
+
+def menuPerimetros():
+    """bienvenido al menu de perimetros
+1. elije la figura que le quieras sacar perimetro
+2. salir """
+
+opcion = input(" ")
+ 
+if opcion <1 or opcion >2:
+    print("opcion invalida intenta denuevo")
+
